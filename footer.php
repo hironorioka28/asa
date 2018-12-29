e9436e72275069e23bd94a16a410fe68f5738b4e
Hironori Oka <hironorioka28@gmail.com>
2018-12-29 22:30:56 +0900

    footer

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
-  <footer>
-    <p>&copy; 鈴木啓 / ASA</p>
-  </footer>
-<?php wp_footer(); ?>
+  <div class="page__content">
+    <footer class="footer">
+      <p class="footer__text">&copy; 鈴木啓 / ASA</p>
+    </footer>
+  </div>
+  <?php wp_footer(); ?>
 </body>
 </html>
